c7a881017178a99ca789aebb516eb86449480be9
Syafrinadyah16 <dyahkusumaw16@gmail.com>
2020-10-31 20:58:03 +0800

    mengupdate login dan menghapus file duplikasi

--- src/main/java/id/kasir/core/Login.java
+++ src/main/java/id/kasir/core/Login.java
@@ -1,27 +1,32 @@
- class password {
+ interface LoginInterface{
+     public void check();
+   }
+
+ class password implements LoginInterface{
    String username, password;
    
    public password (String username,String password) {
          this.username=username;
          this.password=password;
     }
+
    public void check() {
        if(this.username.equals("Novita Khasanah") && (this.password.equals("novita12"))){
-		System.out.println("Welcome");
-		
-	}else {
-		System.out.println("Account not found");
-	}
+    System.out.println("Welcome");
+    
+  }else {
+    System.out.println("Account not found");
+  }
    }
-     
-}
+ }
 
-public class Login {    
+public class Login{    
     public static void main(String[] args){   
         String username = "Novita Khasanah";
         String password = "novita12";
  
-	password masuk=new password(username,password);
+  password masuk=new password(username,password);
         masuk.check();
     }
 }
+
