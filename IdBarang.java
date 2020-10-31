f89d818babf754e5c9a5ff0898d5ccb3e44d8530
IfatusSufairoh <ifatusufairoh@gmail.com>
2020-10-31 16:20:41 +0700

    Update Jumlah Barang Masuk

---
+++ IdBarang.java
@@ -0,0 +1,40 @@
+import java.util.Date;
+
+interface IdBarangInterface{
+public void check () ;
+}
+
+	class ManajemenInventori implements IdBarangInterface{
+	protected int IdBarang;
+	protected int JumlahBarangMasuk; 
+
+	private int correctIdBarang = 5; 
+	public ManajemenInventori(int IdBarang, int JumlahBarangMasuk){
+		this.IdBarang = IdBarang;
+		this.JumlahBarangMasuk = JumlahBarangMasuk; 
+	}
+	
+	public void check(){
+			//validasi Id 
+			if (this.IdBarang == this.correctIdBarang){
+			System.out.println("Id Sesuai");
+		}	else {
+			System.out.println("Id Tidak Sesuai");
+		}
+	}
+}
+
+	public class IdBarang {
+	public static void main(String[] args){
+
+		Date date = new Date();
+		System.out.println(date.toString());
+
+		int inputIdBarang = 5;
+		int inputJumlahBarangMasuk = 100; 
+
+		ManajemenInventori MI = new ManajemenInventori(inputIdBarang, inputJumlahBarangMasuk);
+		MI.check();
+		System.out.println("Barang Yang Masuk 100");
+	}
+}
