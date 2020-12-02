7255b28bd3233a91dfea1dc6fc21e0a5ce4316d8
Syafrinadyah <72138822+Syafrinadyah@users.noreply.github.com>
2020-12-02 20:14:32 +0800

    Update dyah.sql

--- dyah.sql
+++ dyah.sql
@@ -1,94 +1,41 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 02 Des 2020 pada 05.04
--- Versi server: 10.4.6-MariaDB
--- Versi PHP: 7.1.32
+DROP DATABASE IF EXISTS 'db_kasir';
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE DATABASE db_kasir;
+
+USE db_kasir;
+
+DROP TABLE IF EXISTS akun;
+
+CREATE TABLE akun (
+  noId int(6) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username char(50) NOT NULL,
+  email char(50) NOT NULL,
+  password char(30) NOT NULL,
+  leveluser char(15) NOT NULL,
+  INDEX(email)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE = latin1_swedish_ci;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+DROP TABLE IF EXISTS produk;
 
---
--- Database: `dbkasir`
---
+CREATE TABLE produk (
+  kd_barang char(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  nama_barang char(30) NOT NULL,
+  harga_barang int(25) NOT NULL,
+  tgl date NOT NULL,
+  jml_barang_masuk int(15) NOT NULL,
+  jml_barang_keluar int(15) NOT NULL,
+  INDEX(nama_barang)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE = latin1_swedish_ci;
 
--- --------------------------------------------------------
 
---
--- Struktur dari tabel `akun`
---
+DROP TABLE IF EXISTS pembayaran ;
 
-CREATE TABLE `akun` (
-  `noId` int(6) NOT NULL,
-  `username` char(50) NOT NULL,
-  `email` char(50) NOT NULL,
-  `password` char(30) NOT NULL,
-  `leveluser` char(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `akun`
---
-
-INSERT INTO `akun` (`noId`, `username`, `email`, `password`, `leveluser`) VALUES
-(111, 'syafrinadyah', 'syafrinadyah16@gmail.com', 'syafrinadyah16', 'karyawan'),
-(222, 'ifatus22', 'ifatusufairoh22@gmail.com', 'ifatus567', 'karyawan'),
-(333, 'novitakhasanah33', 'novitakhasanah12@gmail.com', 'novi12', 'admin'),
-(444, 'kevinnn', 'kevinprianka@gmail.com', 'kevinn987', 'karyawan');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `produk`
---
-
-CREATE TABLE `produk` (
-  `kd_barang` char(10) NOT NULL,
-  `nama_barang` char(30) NOT NULL,
-  `harga_barang` int(25) NOT NULL,
-  `tgl` date NOT NULL,
-  `jml_barang_masuk` int(15) NOT NULL,
-  `jml_barang_keluar` int(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `produk`
---
-
-INSERT INTO `produk` (`kd_barang`, `nama_barang`, `harga_barang`, `tgl`, `jml_barang_masuk`, `jml_barang_keluar`) VALUES
-('A001', 'NESCAFE', 7000, '2020-05-01', 200, 50),
-('B001', 'NUTRIVE', 8000, '2020-06-17', 500, 200),
-('C003', 'FRESTEA', 6200, '2020-02-22', 500, 200),
-('D004', 'SHINZUI', 29900, '2019-12-25', 300, 20),
-('E005', 'CIMORY', 14000, '2020-07-15', 200, 10);
-
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `akun`
---
-ALTER TABLE `akun`
-  ADD PRIMARY KEY (`noId`);
-
---
--- Indeks untuk tabel `produk`
---
-ALTER TABLE `produk`
-  ADD PRIMARY KEY (`kd_barang`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE pembayaran (
+  noId_pembayaran int(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  kd_barang char(10) NOT NULL,
+  nama_barang char(30) NOT NULL,
+  harga_barang int(25) NOT NULL,
+  tgl_pembayaran date NOT NULL,
+  FOREIGN KEY (kd_barang) REFERENCES produk (kd_barang) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE = latin1_swedish_ci;
