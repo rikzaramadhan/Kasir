c40685b1f31f62429342af92b601105c6b2ba269
rikzaramadhan <rikzaoke2@gmail.com>
2021-01-06 18:23:27 +0700

    melanjutkan fitur absensi

---
+++ common/AbsenService.java
@@ -0,0 +1,9 @@
+package common;
+
+import model.Absent;
+
+public interface AbsenService {
+
+    boolean insertAbsent(Absent absent) throws Exception;
+//    void updateAbsent(Absent absent) throws Exception;
+}
